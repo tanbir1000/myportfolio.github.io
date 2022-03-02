e44475f9aa16beac33479e140411f14d426916be
Tanbir Akhtar <84980703+tanbir1000@users.noreply.github.com>
2022-03-02 22:25:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -16,3 +16,9 @@ $('.menu-btn').click(function(){
 });
 
 });
+var typed = new Typed(".typing", {
+    strings: ["Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
